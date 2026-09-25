b9903b4f5dec2b005cf15d96687bda4e0aa2f507
dev <dev@example.com>
2026-09-25 14:26:53 +0800

    fix: link QuartzCore

--- MXYZFCheat.m
+++ MXYZFCheat.m
@@ -73,7 +73,7 @@ static UIView *mx_rainbow_ball(CGFloat size) {
     CGPathAddEllipseInRect(p, NULL, CGRectMake(0, 0, size, size));
     CGPathAddEllipseInRect(p, NULL, CGRectMake((size-hole)/2, (size-hole)/2, hole, hole));
     mask.path = p;
-    mask.fillRule = (CAFillRule)1; // kCAFillRuleEvenOdd
+    mask.fillRule = kCAFillRuleEvenOdd;
     CGPathRelease(p);
     grad.mask = mask;
 
